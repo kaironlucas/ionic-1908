a6a16c191a3853dc70c08eb2ab13896d68c33b34
Kairon <34685175+kaironlucas@users.noreply.github.com>
2019-08-19 20:03:54 -0300

    Update home.ts

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import postos from "./postos.json"
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+postos:[];
 
   constructor(public navCtrl: NavController) {
-
+      this.postos = postos.postos;
+      console.log(postos)
   }
 
 }
