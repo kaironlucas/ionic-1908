81e0b5433bf745321f64a348046754ba7750ddef
kaironlucas <noriakxp@gmail.com>
2019-08-21 17:34:24 -0300

    1 file modified

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -9,9 +9,23 @@ import postos from "./postos.json"
 export class HomePage {
 postos:[];
 
+nome:string;
+bairro:string;
+nota:number;
+
   constructor(public navCtrl: NavController) {
       this.postos = postos.postos;
       console.log(postos)
+  }
+
+  env(){
+      var novo = new postos{
+        nome:string,
+        bairro:string,
+        nota:number,
+      }
+    postos.push(novo);
+
   }
 
 }
